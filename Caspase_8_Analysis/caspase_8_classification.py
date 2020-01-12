4b520dd12c4cfeb29ef7a759ab03a8263e21fe75
Mike Xydas <mikexydas@gmail.com>
2020-01-13 00:56:43 +0200

    Added gridsearch.

--- Caspase_8_Analysis/caspase_8_classification.py
+++ Caspase_8_Analysis/caspase_8_classification.py
@@ -54,7 +54,7 @@ def split_dataset(df, training_rows, feature_indexes):
     features_df = train_df_X
 
     # Calculate ANOVA f value and select top-k features
-    ANOVA_feature_selector = SelectKBest(f_classif, k=5).fit(features_df, train_df_Y)
+    ANOVA_feature_selector = SelectKBest(f_classif, k=4).fit(features_df, train_df_Y)
     final_features_indexes = ANOVA_feature_selector.get_support(indices=True)
     features_df = pd.DataFrame(ANOVA_feature_selector.transform(features_df))
     pd.DataFrame([ANOVA_feature_selector.scores_], columns=temp_cols)\
@@ -75,7 +75,7 @@ def split_dataset(df, training_rows, feature_indexes):
     test_df_X = pca.transform(test_df_X)    # Apply the transformation on the test set
     # Save components and variances in a csv
     comps = pd.concat([comps, variances], axis=1)
-    # comps.to_csv(path_or_buf='resources/PCA_components_MD_3D_no_20_23_NULL.csv', sep=',')
+    comps.to_csv(path_or_buf='resources/PCA_components_MD_3D_no_20_23_NULL.csv', sep=',')
 
     # This will result in a plot if remaining features of features_df = 2
     plot_scatter_points(pd.concat([features_df, train_df_Y], axis=1), [0, 1],
@@ -94,15 +94,15 @@ features_selected = feature_groups['MD'] + feature_groups['3D']
 train_df_X, train_df_Y, test_df_X, test_df_Y = split_dataset(df, 29, features_selected)
 
 # Fitting on chosen method
-#clf = RandomForestClassifier(class_weight={1:5}, n_estimators=1, criterion='entropy',
+# clf = RandomForestClassifier(class_weight={1:6}, n_estimators=1, criterion='entropy',
 #                                max_features='sqrt', max_depth=60)
 # clf = RandomForestClassifier()
 # clf = SVC(class_weight={1:6}, gamma='auto', tol=1e-5)
-# clf = SVC()
+clf = SVC()
 # clf = LogisticRegression(class_weight={1:5}, solver='liblinear', penalty='l1', tol=5e-5, max_iter=10000)
 # clf = LogisticRegression()
 # clf = KNeighborsClassifier(n_neighbors=1, algorithm='brute', p=2)
-clf = KNeighborsClassifier()
+# clf = KNeighborsClassifier()
 
 clf.fit(train_df_X, train_df_Y)
 
@@ -115,7 +115,34 @@ do_evaluations(test_df_Y, pred_y, None, probs_exist=False, displayed_name="Paper
 #plot_confusion(clf, "Caspase_8 Confusion", test_df_X, test_df_Y, ["Inactive", "Active"],
 #               show_plot=False, print_confusion=True)
 
-# Run Leave One Out cross-validation on training set
-pred_y = performLOO(clf, train_df_X, train_df_Y, probsExist=False)
+# # Run Leave One Out cross-validation on training set
+# pred_y = performLOO(clf, train_df_X, train_df_Y, probsExist=False)
 
 
+# # Number of trees in random forest
+# n_estimators = [int(x) for x in np.linspace(start = 1, stop = 100, num = 2)]
+# # Number of features to consider at every split
+# max_features = ['auto', 'sqrt']
+# # Maximum number of levels in tree
+# max_depth = [int(x) for x in np.linspace(5, 110, num = 5)]
+# max_depth.append(None)
+# # Minimum number of samples required to split a node
+# min_samples_split = [1, 2, 3, 5, 10]
+# # Minimum number of samples required at each leaf node
+# min_samples_leaf = [1, 2, 3, 4, 5]
+# # Method of selecting samples for training each tree
+# bootstrap = [True, False]
+# class_weight=[{1:5}, {1:4}, {1:3}, {1:2}, {1:1}, {1:6}, {1:7}]
+# # Create the random grid
+# random_grid = {'n_estimators': n_estimators,
+#                'max_features': max_features,
+#                'max_depth': max_depth,
+#                'min_samples_split': min_samples_split,
+#                'min_samples_leaf': min_samples_leaf,
+#                'bootstrap': bootstrap,
+#                'class_weight': class_weight
+#                }
+#
+# rf_random = RandomizedSearchCV(estimator = clf, param_distributions = random_grid, n_iter = 1000, cv = 3, verbose=2, random_state=42, n_jobs = -1, scoring='f1')
+# rf_random.fit(train_df_X, train_df_Y)
+# print(rf_random.best_params_)
